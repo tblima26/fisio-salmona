88919cca264415f246b0482e4dd00697f4da1872
Thiago Borges <you@example.com>
2025-03-10 20:50:02 -0300

    Google Fonts installed
    Google fonts are installed and i can use.

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1>Hello</h1>
-        <span>hellow world!</span>
+      <div className="w-full h-32 flex flex-col items-center justify-center bg-slate-500">
+        <h1 className='text-5xl text-white font-jetbrains' >Hello</h1>
+        <span className='text-2xl text-neutral-300 font-lato' >hellow world!</span>
       </div>
     </>
   )
