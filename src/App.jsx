f148c65184712aff3ceaafed57c5366e772d5f81
Thiago Borges <you@example.com>
2025-03-11 19:31:29 -0300

    Lucide React
    Lucide React Icons Installed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 
+import { Cherry, Icon } from 'lucide-react';
 import './App.css'
 import clinicImage from './images/clinic.jpg';
 
@@ -14,7 +15,8 @@ function App() {
       </div>
       <img src={clinicImage} alt='clinic' className='h-screen object-cover w-full' />
       <div className="w-full h-32 flex flex-col items-center justify-center bg-slate-500">
-        <h1 className='text-5xl text-white font-jetbrains' >Proximos Elementos</h1>
+        <h1 className='text-5xl text-white font-jetbrains flex items-center gap-5' ><Cherry size={30} />Proximos Elementos <Cherry size={30} /></h1>
+
         <span className='text-2xl text-neutral-300 font-lato' >Crie novos elementos aqui</span>
       </div>
     </>
