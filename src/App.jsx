ebfb172d0d88b028bf9566d8c6bf8ad2bffb8caf
Thiago Borges <you@example.com>
2025-03-11 19:37:51 -0300

    Initial layout
    Implemented initial homepage layout with a header, greeting text, and an image. Added a placeholder for future elements with a styled section featuring an icon.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { Cherry, Icon } from 'lucide-react';
+import { Cherry } from 'lucide-react';
 import './App.css'
 import clinicImage from './images/clinic.jpg';
 
@@ -11,13 +11,13 @@ function App() {
     <>
       <div className="w-full h-32 flex flex-col items-center justify-center bg-slate-500">
         <h1 className='text-5xl text-white font-jetbrains' >Hello</h1>
-        <span className='text-2xl text-neutral-300 font-lato' >hellow world!</span>
+        <span className='text-2xl text-neutral-300 font-lato' >hello world!</span>
       </div>
       <img src={clinicImage} alt='clinic' className='h-screen object-cover w-full' />
       <div className="w-full h-32 flex flex-col items-center justify-center bg-slate-500">
-        <h1 className='text-5xl text-white font-jetbrains flex items-center gap-5' ><Cherry size={30} />Proximos Elementos <Cherry size={30} /></h1>
+        <h1 className='text-5xl text-white font-jetbrains flex items-center gap-5' ><Cherry size={30} />Next  Elements <Cherry size={30} /></h1>
 
-        <span className='text-2xl text-neutral-300 font-lato' >Crie novos elementos aqui</span>
+        <span className='text-2xl text-neutral-300 font-lato' >Put your new elements here!!!!</span>
       </div>
     </>
   )
